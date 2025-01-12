dacd00b1603566a98b79201ab934ffffcaf1dfd9
Ananya <iyerananya999@gmail.com>
2025-01-12 16:29:12 -0600

    Added servers

---
+++ ftp.py
@@ -0,0 +1,12 @@
+from twisted.cred.checkers import AllowAnonymousAccess, InMemoryUsernamePasswordDatabaseDontUse
+from twisted.cred.portal import Portal
+from twisted.internet import reactor
+from twisted.protocols.ftp import FTPFactory, FTPRealm
+
+def run_ftp():
+    portal = Portal(FTPRealm("./public"), [AllowAnonymousAccess()])
+    factory = FTPFactory(portal)
+    reactor.listenTCP(21, factory)
+    reactor.run()
+
+run_ftp()
